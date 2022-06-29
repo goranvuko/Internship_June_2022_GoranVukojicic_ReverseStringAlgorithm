3e1be977313d0d4829923d509fc7ed93a3b950df
Goran Vukojičić <goranvukojicic@gmail.com>
2022-06-29 15:02:47 +0200

    Add project files.

---
+++ reverseStringAlgorithm/ReverseString.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reverseStringAlgorithm
+{
+    public class ReverseString
+    {
+        public static string reverseMethod()
+        {
+            string str = ""; 
+            string revString = "";
+            
+            Console.WriteLine("Enter string:");
+            str = Console.ReadLine();
+
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                revString += str[i];
+            }
+            
+            return revString;
+            
+        }
+    }
+}
